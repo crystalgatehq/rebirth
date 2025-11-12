3848abb732a0d0b8272b8cf10d451d6bea85af76
wagura Maurice <buisness@waguramaurice.com>
2025-11-12 03:35:14 +0300

    update 2025_11_08_160505_create_counties_table.php

--- database/migrations/2025_11_08_160505_create_counties_table.php
+++ database/migrations/2025_11_08_160505_create_counties_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('counties', function (Blueprint $table) {
             $table->id();
-            $table->uuid('uuid')->unique();
+            $table->string('code')->unique();
             $table->string('name');
             $table->string('slug')->nullable()->unique();
             $table->string('description')->nullable();
